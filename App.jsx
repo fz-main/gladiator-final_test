158a0ace9894331703166dc103c56848caadcf26
Veloweb <velowebfz@proton.me>
2026-05-12 23:12:06 +0300

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,7 @@
+const GlobalStyles = () => (
+  <style>{`
+    *, *::before, *::after { margin:0; padding:0; box-sizing:border-box; }
+    html, body { width:100vw; overflow-x:hidden; position:relative; background:#0a0a0a; font-family:'Space Mono', monospace; color:#fff; }
+    #root { position:relative; width:100%; }
+  `}</style>
+);
